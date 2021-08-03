88b47d07ff7e9d7d4d468b9a0eda4557dd4eda29
Kien Dang <mail@kien.ai>
2021-08-04 03:52:58 +0800

    Add completion for py.Dynamic.global

--- amm/compiler/src/main/scala-2/ammonite/compiler/Pressy.scala
+++ amm/compiler/src/main/scala-2/ammonite/compiler/Pressy.scala
@@ -163,7 +163,64 @@ object Pressy {
         }
     }
 
-    def prefixed: (Int, Seq[(String, Option[String])]) = tree match {
+    def prefixed: (Int, Seq[(String, Option[String])]) = {
+      val scalapyCompletion = {
+        import scala.util.Try
+        import me.shadaj.scalapy.py
+        import me.shadaj.scalapy.py.PyQuote
+        import pressy._
+
+        val extract = new ExtractTree { val universe: pressy.type = pressy }
+
+        tree match {
+          case t @ q"""${expr @ extract.SelectDynamicChain(root, attrs)}
+            .selectDynamic(${Literal(Constant(prefix))})
+            """ if pressy.ask(() => t.tpe <:< pressy.typeOf[py.Any]) =>
+              val offset = expr.pos.end + 1
+              val prefixStr = {
+                val s = prefix.toString
+                if (s == "<error>") "" else s
+              }
+
+              root match {
+                case _ if pressy.ask(() => root.tpe =:= pressy.typeOf[py.Dynamic.global.type]) =>
+
+                  attrs match {
+                    case Nil if !prefixStr.isEmpty =>
+                      val matches = py.module("rlcompleter")
+                        .Completer(py"globals()")
+                        .global_matches(prefixStr)
+                        .as[Seq[String]]
+                        .filter(_.startsWith(prefixStr))
+
+                      Some(offset, matches.map((_, None)))
+
+                    case _ :: _ =>
+                      val exprStr = attrs.mkString(".") + "."
+                      val text = exprStr + prefixStr
+                      val pattern = s"^${exprStr}(${prefixStr}.*)$$".r
+                      val matches = py.module("rlcompleter")
+                        .Completer(py"globals()")
+                        .attr_matches(text)
+                        .as[Seq[String]]
+                        .map(pattern.findFirstMatchIn(_).map(_.subgroups))
+                        .collect { case Some(s :: Nil) => s }
+
+                      Some(offset, matches.map((_, None)))
+
+                    case _ => None
+                  }
+
+                case _ => None
+              }
+          case _ => None
+        }
+      }
+
+      scalapyCompletion match {
+        case Some(result) => result
+        case None => tree match {
+
       case t @ pressy.Select(qualifier, name) =>
 
         val dotOffset = if (qualifier.pos.point == t.pos.point) 0 else 1
@@ -249,6 +306,7 @@ object Pressy {
           comps.filter(m => !blacklisted(m.sym))
                .map { s => (memberToString(s), None) }
         )
+      }}
     }
     def ask(index: Int, query: (Position, Response[List[pressy.Member]]) => Unit) = {
       val position = new OffsetPosition(currentFile, index)
@@ -341,6 +399,33 @@ object Pressy {
     def shutdownPressy() = {
       Option(cachedPressy).foreach(_.askShutdown())
       cachedPressy = null
+    }
+  }
+
+  trait ExtractTree {
+    val universe: scala.reflect.api.Universe
+    import universe._
+
+    object SelectChain {
+      def unapply(t: Tree): Option[(Tree, List[String])] = {
+        val r = t match {
+          case q"${SelectChain(q, attrs)}.${TermName(attrN)}" =>
+            Some((q, attrN.toString :: attrs))
+          case _ => Some(t, Nil)
+        }
+        r.collect { case (q, attrs) => (q, attrs.reverse) }
+      }
+    }
+
+    object SelectDynamicChain {
+      def unapply(t: Tree): Option[(Tree, List[String])] = {
+        val r = t match {
+          case q"${SelectDynamicChain(q, args)}.selectDynamic(${Literal(Constant(argN))})" =>
+            Some((q, argN.toString :: args))
+          case _ => Some(t, Nil)
+        }
+        r.collect { case (q, args) => (q, args.reverse) }
+      }
     }
   }
 }
