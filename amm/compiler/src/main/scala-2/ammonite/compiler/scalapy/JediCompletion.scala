ba20148a84703377ff967fd178f356d33e439be0
Kien Dang <mail@kien.ai>
2021-08-26 00:13:24 +0800

    Add jedi completion for py.Dynamic.global

--- amm/compiler/src/main/scala-2/ammonite/compiler/scalapy/JediCompletion.scala
+++ amm/compiler/src/main/scala-2/ammonite/compiler/scalapy/JediCompletion.scala
@@ -9,7 +9,7 @@ import me.shadaj.scalapy.py
 import me.shadaj.scalapy.py.PyQuote
 
 trait JediCompletion extends Completion {
-  import global._
+  import global.{ApplyDynamic => _, _}
   import JediCompletion._
 
   def complete(
@@ -18,10 +18,77 @@ trait JediCompletion extends Completion {
     allCode: String,
     index: Int
   ): Option[(Int, Seq[(String, Option[String])])] = jedi.toOption.flatMap { jedi =>
-    None
+    tree match {
+      case t @ q"""${expr @ DynamicChain(root, dynamics)}
+        .selectDynamic(${Literal(Constant(prefix))})
+        """ if global.ask(() => t.tpe <:< global.typeOf[py.Any]) =>
+          val offset = expr.pos.end + 1
+          val prefixStr = {
+            val s = prefix.toString
+            if (s == "<error>") "" else s
+          }
+
+          root match {
+            case _ if global.ask(() => root.tpe =:= global.typeOf[py.Dynamic.global.type]) =>
+              val exprStr = convertToPython(dynamics)
+              val code = if (exprStr.isEmpty) prefixStr else exprStr + "." + prefixStr
+              val completions = getCompletions(jedi)(code)
+              Some(offset, completions.map((_, None)))
+
+            case _ => None
+          }
+
+      case _ => None
+    }
+  }
+
+  object DynamicChain {
+    private object Aux {
+      def unapply(t: Tree): Option[(Tree, List[Dynamic])] = t match {
+        case q"${Aux(q, ds)}.selectDynamic(${Literal(Constant(term))})" =>
+          Some(q, SelectDynamic(term.toString) :: ds)
+        case q"${Aux(q, ds)}.applyDynamic(${Literal(Constant(method))})()" =>
+          Some(q, ApplyDynamic(method.toString) :: ds)
+        case q"${Aux(q, ds)}.applyDynamic(${Literal(Constant(method))})($_)" =>
+          Some(q, ApplyDynamic(method.toString) :: ds)
+        case q"${Aux(q, ds)}.applyDynamicNamed(${Literal(Constant(method))})($_)" =>
+          Some(q, ApplyDynamicNamed(method.toString) :: ds)
+        case q"${Aux(q, ds)}.bracketAccess($_)" =>
+          Some(q, BracketAccess :: ds)
+        case _ => Some(t, Nil)
+      }
+    }
+
+    def unapply(t: Tree): Option[(Tree, List[Dynamic])] = t match {
+      case Aux(t, ds) => Some(t, ds.reverse)
+      case _ => None
+    }
   }
 }
 
 object JediCompletion {
   val jedi = Try(py.module("jedi"))
+
+  def getCompletions(jedi: py.Module)(code: String): Seq[String] =
+    py"list(map($getName, ${jedi.Interpreter(code, py"[$namespace]").complete()}))".as[Seq[String]]
+
+  private val getName = py.module("operator").attrgetter("name")
+
+  def namespace = py"globals()"
+
+  sealed trait Dynamic
+  case class SelectDynamic(term: String) extends Dynamic
+  case class ApplyDynamic(method: String) extends Dynamic
+  case class ApplyDynamicNamed(method: String) extends Dynamic
+  case object BracketAccess extends Dynamic
+
+  def convertToPython(dynamic: Dynamic): String = dynamic match {
+    case SelectDynamic(term) => term
+    case ApplyDynamic(method) => method + "()"
+    case ApplyDynamicNamed(method) => method + "()"
+    case BracketAccess => "__getitem__()"
+  }
+
+  def convertToPython(dynamics: Seq[Dynamic]): String =
+    dynamics.map(convertToPython).mkString(".")
 }
